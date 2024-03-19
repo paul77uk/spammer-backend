95b317f9e5d60b32a440f10fbbed71c9687268eb
paul77uk <paul77uk@gmail.com>
2024-03-19 18:35:25 +0000

    allowing cors

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import { headers } from "next/headers.js";
 
 export const GET = async () => {
   const posts = await prisma.post.findMany();
@@ -10,7 +11,12 @@ export const GET = async () => {
 };
 
 export const POST = async (req) => {
-  
+  options = {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  };
   try {
     const { text } = await req.json();
     if (!text) {
