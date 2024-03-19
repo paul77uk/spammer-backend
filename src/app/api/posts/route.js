efa1575b61c68adf121591805ac7b54f72fb871f
paul77uk <paul77uk@gmail.com>
2024-03-19 17:34:00 +0000

    allowing cors

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-import NextCors from "nextjs-cors";
 
 export const GET = async () => {
   const posts = await prisma.post.findMany();
@@ -11,12 +10,7 @@ export const GET = async () => {
 };
 
 export const POST = async (req) => {
-  await NextCors(req, res, {
-    // Options
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: "*",
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  });
+  
   try {
     const { text } = await req.json();
     if (!text) {
